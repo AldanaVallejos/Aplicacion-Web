a0595e526ed05d314f5a7cab77826d3c5bd36827
Aldana  Vallejos <92132412+AldanaVallejos@users.noreply.github.com>
2023-03-05 20:04:20 -0300

    Add files via upload

---
+++ scripts (2022_11_18 20_57_08 UTC).js
@@ -0,0 +1,44 @@
+// Declaro evento para el primer boton de añadir al carrito
+let button = document.getElementById("botonCarrito");
+
+function hizoClick() {
+    alert("Se añadió el producto a su carrito de compras.");
+}
+
+button.addEventListener("click", hizoClick);
+
+// Declaro evento para el segundo boton de añadir al carrito
+let buttonUno = document.getElementById("botonCarritoUno");
+
+function hizoClick() {
+    alert("Se añadió el producto a su carrito de compras.");
+}
+
+buttonUno.addEventListener("click", hizoClick);
+
+// Declaro evento para el tercer boton de añadir al carrito
+let buttonDos = document.getElementById("botonCarritoDos");
+
+function hizoClick() {
+    alert("Se añadió el producto a su carrito de compras.");
+}
+
+buttonDos.addEventListener("click", hizoClick);
+
+// Declaro evento para el cuarto boton de añadir al carrito
+let buttonCuatro = document.getElementById("botonCarritoCuatro");
+
+function hizoClick() {
+    alert("Se añadió el producto a su carrito de compras.");
+}
+
+buttonCuatro.addEventListener("click", hizoClick);
+
+// Declaro evento para el quinto boton de añadir al carrito
+let buttonCinco = document.getElementById("botonCarritoCinco");
+
+function hizoClick() {
+    alert("Se añadió el producto a su carrito de compras.");
+}
+
+buttonCinco.addEventListener("click", hizoClick);
